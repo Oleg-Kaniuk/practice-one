8b975f42edebf81b0e6646e2735839f73c0bd87d
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-08-27 23:05:23 +0300

    add fix

--- src/index.js
+++ src/index.js
@@ -6,41 +6,41 @@
 // https://www.npmjs.com/package/tui-pagination
 
 
-import { pagination } from './pagination';
+import { pagination } from './pagination';  // Імпортуємо функцію pagination з іншого файлу
 
-const API_KEY = 'uHSLi07StIOlriMPxJGxUbSYsHDs6AFx';
-const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
-const list = document.querySelector('.gallery');
-const page = pagination.getCurrentPage();
-const form = document.querySelector('.form');
-let query = '';
+const API_KEY = 'uHSLi07StIOlriMPxJGxUbSYsHDs6AFx';  // API-ключ для доступу до Ticketmaster API
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';  // Базова URL-адреса API
+const list = document.querySelector('.gallery');  // Вибираємо DOM-елемент з класом '.gallery'
+const page = pagination.getCurrentPage();  // Отримуємо поточну сторінку з об'єкту pagination
+const form = document.querySelector('.form');  // Вибираємо форму пошуку за класом '.form'
+let query = '';  // Змінна для зберігання пошукового запиту
 
-const backdrop = document.querySelector('.backdrop');
-const modal = document.querySelector('.modal');
+const backdrop = document.querySelector('.backdrop');  // Вибираємо DOM-елемент з класом '.backdrop'
+const modal = document.querySelector('.modal');  // Вибираємо DOM-елемент з класом '.modal'
 
-form.addEventListener('submit', onFormSearch)
+form.addEventListener('submit', onFormSearch);  // Додаємо обробник події 'submit' до форми пошуку
 
 function onFormSearch(evt) {
-    evt.preventDefault();
-    console.log(evt);
-    query = evt.target.elements.input.value;
+    evt.preventDefault();  // Перешкоджаємо стандартній поведінці форми
+    console.log(evt);  // Виводимо об'єкт події у консоль
+    query = evt.target.elements.input.value;  // Отримуємо значення поля вводу і зберігаємо в змінну 'query'
 
-    renderFirstPage(page, query);
+    renderFirstPage(page, query);  // Викликаємо функцію для відображення першої сторінки з результатами пошуку
 }
 
-list.addEventListener('click', onItemClick)
+list.addEventListener('click', onItemClick);  // Додаємо обробник події 'click' до списку з результатами
 
 function onItemClick(evt) {
-    const id = evt.target.id;
-    console.log(id);
-    backdrop.classList.remove('is-hidden');
-    createModalMarkup(id)
+    const id = evt.target.id;  // Отримуємо ідентифікатор (id) елементу, на який натиснули
+    console.log(id);  // Виводимо ідентифікатор у консоль
+    backdrop.classList.remove('is-hidden');  // Видаляємо клас 'is-hidden' з елементу '.backdrop'
+    createModalMarkup(id);  // Викликаємо функцію для створення вмісту модального вікна
 }
 
-backdrop.addEventListener('click', onCloseModal)
+backdrop.addEventListener('click', onCloseModal);  // Додаємо обробник події 'click' до елементу '.backdrop'
 
 function onCloseModal() {
-    backdrop.classList.add('is-hidden')
+    backdrop.classList.add('is-hidden');  // Додаємо клас 'is-hidden' до елементу '.backdrop', приховуючи його
 }
 
 function fetchDataId(id) {
@@ -48,25 +48,25 @@ function fetchDataId(id) {
     .then(res => {
       // console.log(res);
       if (!res.ok) {
-        throw new Error();
+        throw new Error();  // Викидаємо помилку, якщо відповідь сервера не "ок"
       }
-      return res.json();
+      return res.json();  // Повертаємо відповідь сервера у форматі JSON
     })
     .catch(rej => {
-      console.log(rej);
+      console.log(rej);  // Виводимо помилку у консоль
     });
 }
 
 function createModalMarkup(id) {
-    fetchDataId()
+    fetchDataId(id)
         .then((data) => {
-            console.log(data);
-            const markup = `<img src="${data._embedded.events[0].images[0].url}"
-             alt="${data._embedded.events[0].name}">
-             <p>${data._embedded.events[0].name}</p>`
+            console.log(data);  // Виводимо отримані дані у консоль
+          const markup = `<img src="${data._embedded.events[0].images[0].url}" 
+          alt="${data._embedded.events[0].name}">
+          <p>${data._embedded.events[0].name}</p>`;  // Створюємо розмітку для модального вікна
 
-            modal.innerHTML = markup;
-    })
+        modal.innerHTML = markup;  // Вставляємо створену розмітку в модальне вікно
+    });
 }
 
 function fetchData(page, query) {
@@ -74,47 +74,49 @@ function fetchData(page, query) {
     .then(res => {
       // console.log(res);
       if (!res.ok) {
-        throw new Error();
+        throw new Error();  // Викидаємо помилку, якщо відповідь сервера не "ок"
       }
-      return res.json();
+      return res.json();  // Повертаємо відповідь сервера у форматі JSON
     })
     .catch(rej => {
-      console.log(rej);
+      console.log(rej);  // Виводимо помилку у консоль
     });
 }
+
+renderFirstPage(page, query);  // Викликаємо функцію для відображення першої сторінки з результатами пошуку
 
 function renderFirstPage(page, query) {
   fetchData(page, query)
     .then(data => {
-      pagination.reset(data.page.totalElements);
-      createMarkup(data._embedded.events);
+      pagination.reset(data.page.totalElements);  // Скидаємо параметри пагінації із загальною кількістю елементів
+      createMarkup(data._embedded.events);  // Викликаємо функцію для створення розмітки зі списком подій
     })
     .catch(err => {
-      console.log(err);
+      console.log(err);  // Виводимо помилку у консоль
     });
 }
-renderFirstPage(page, query);
 
 function createMarkup(arr) {
   const markup = arr
     .map(
         ({ name, id }) =>
             `<li id="${id}"><p id="${id}">${name}</p></li>`
-    ).join('');
-  list.innerHTML = markup;
+    ).join('');  // Створюємо розмітку для списку подій
+  list.innerHTML = markup;  // Вставляємо створену розмітку у список
 }
+
 function renderEvt(page, query) {
   fetchData(page, query)
     .then(data => {
-      createMarkup(data._embedded.events);
+      createMarkup(data._embedded.events);  // Викликаємо функцію для створення розмітки зі списком подій
     })
     .catch(err => {
-      console.log(err);
+      console.log(err);  // Виводимо помилку у консоль
     });
 }
-pagination.on('afterMove', (event) => {
-  const currentPage = event.page;
-  renderEvt(currentPage, query)
-  console.log(currentPage);
-});
 
+pagination.on('afterMove', (event) => {
+  const currentPage = event.page;  // Отримуємо номер поточної сторінки з об'єкту події
+  renderEvt(currentPage, query);  // Викликаємо функцію для відображення списку подій на новій сторінці
+  console.log(currentPage);  // Виводимо номер поточної сторінки у консоль
+});
